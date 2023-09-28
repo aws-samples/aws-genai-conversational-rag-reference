88907586c79b46e2f9db2e62dbdedcc836102331
Gergely Varga <gergelyv@amazon.com>
2023-09-28 13:43:24 +0800

    refactor(projen): split framework constructs to their own files

--- projenrc/framework/galileo-sdk.ts
+++ projenrc/framework/galileo-sdk.ts
@@ -1,56 +1,15 @@
 /*! Copyright [Amazon.com](http://amazon.com/), Inc. or its affiliates. All Rights Reserved.
 PDX-License-Identifier: Apache-2.0 */
 import { MonorepoTsProject } from "@aws/pdk/monorepo";
-import { AwsCdkConstructLibrary } from "projen/lib/awscdk";
 import { Stability } from "projen/lib/cdk";
-import { EsmTypescriptProject } from "./components/esm-typescript";
-import {
-  AWS_SDK_VERSION,
-  CDK_VERSION,
-  LANGCHAIN_VERSION,
-  PROJECT_AUTHOR,
-  DEFAULT_RELEASE_BRANCH,
-  PDK_VERSION,
-} from "./constants";
-
-export class GalileoCdkLib extends AwsCdkConstructLibrary {
-  constructor(monorepo: MonorepoTsProject) {
-    super({
-      ...PROJECT_AUTHOR,
-      parent: monorepo,
-      stability: Stability.EXPERIMENTAL,
-      packageManager: monorepo.package.packageManager,
-      name: "@aws-galileo/galileo-cdk-lib",
-      outdir: "packages/galileo-cdk-lib",
-      cdkVersion: CDK_VERSION,
-      constructsVersion: "10.2.52",
-      jsiiVersion: "5.x",
-      defaultReleaseBranch: DEFAULT_RELEASE_BRANCH,
-      deps: [`@aws/pdk@^${PDK_VERSION}`],
-      publishDryRun: true,
-    });
-
-    // TODO: remove this once we start using this framework lib
-    // for now just going to start dumping some snippets in here
-    // and don't want to slow development of the demo as dep
-    // @ts-ignore - private
-    this.buildTask._locked = false;
-    this.buildTask.reset('echo "disabling build until we use it"');
-
-    // this.package.addPackageResolutions("jsii-rosetta@5.x");
-  }
-}
+import { EsmTypescriptProject } from "../components/esm-typescript";
+import { AWS_SDK_VERSION, LANGCHAIN_VERSION } from "../constants";
 
 // TODO: make this Jsii project so we can vend python and other languages automatically
 // Requires bundling all non-Jsii deps and ensure specific interface rules, so waiting till working in Ts
 export class GalileoSdk extends EsmTypescriptProject {
   constructor(monorepo: MonorepoTsProject) {
     super({
-      parent: monorepo,
-      stability: Stability.EXPERIMENTAL,
-      packageManager: monorepo.package.packageManager,
-      name: "@aws-galileo/galileo-sdk",
-      outdir: "packages/galileo-sdk",
       // jsiiVersion: "5.x",
       deps: [
         "@aws-crypto/sha256-js",
@@ -66,7 +25,10 @@ export class GalileoSdk extends EsmTypescriptProject {
         "uuid",
         `langchain@${LANGCHAIN_VERSION}`, // not semver so need to pin
       ],
+      depsToTransform: ["safe-handlebars"],
       devDeps: ["@types/uuid", "@types/lodash", "aws-sdk-client-mock"],
+      packageManager: monorepo.package.packageManager,
+      parent: monorepo,
       peerDeps: [
         "@aws-sdk/protocol-http",
         "@aws-sdk/querystring-parser",
@@ -81,11 +43,13 @@ export class GalileoSdk extends EsmTypescriptProject {
         `@aws-sdk/rds-signer@^${AWS_SDK_VERSION}`,
         `@aws-sdk/types@^${AWS_SDK_VERSION}`,
       ],
-      depsToTransform: ["safe-handlebars"],
       publishDryRun: true,
+      name: "@aws-galileo/galileo-sdk",
+      outdir: "packages/galileo-sdk",
       // TODO: once we marshal the root module exports better for cross-env/modules we can re-enable this
       // currently will not support root import of the module
       rootExport: false,
+      stability: Stability.EXPERIMENTAL,
     });
   }
 }
---
+++ projenrc/framework/index.ts
@@ -0,0 +1,4 @@
+/*! Copyright [Amazon.com](http://amazon.com/), Inc. or its affiliates. All Rights Reserved.
+PDX-License-Identifier: Apache-2.0 */
+export * from "./galileo-cdk-lib";
+export * from "./galileo-sdk";
