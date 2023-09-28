c958d5745be8ef18256719eda2291041b51a3b33
Gergely Varga <gergelyv@amazon.com>
2023-09-28 14:27:30 +0800

    feat(cli): add galileo-cli project

--- packages/galileo-cli/src/index.ts
+++ packages/galileo-cli/src/index.ts
@@ -1,5 +1,3 @@
 /*! Copyright [Amazon.com](http://amazon.com/), Inc. or its affiliates. All Rights Reserved.
 PDX-License-Identifier: Apache-2.0 */
-export * from "./galileo-cdk-lib";
-export * from "./galileo-cli";
-export * from "./galileo-sdk";
+export { run } from "@oclif/core";
