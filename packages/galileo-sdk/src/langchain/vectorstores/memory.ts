417fb3b2019a4436e639aa0de2d879bdf1a2b3de
Marten Payne <martenp@amazon.com>
2023-10-23 12:58:47 +1100

    chore(unused): remove unused code
    - identified in cleanup sweep

--- packages/galileo-sdk/src/langchain/vectorstores/memory.ts
+++
@@ -1,4 +0,0 @@
-/*! Copyright [Amazon.com](http://amazon.com/), Inc. or its affiliates. All Rights Reserved.
-PDX-License-Identifier: Apache-2.0 */
-// TODO: just exporting this for testing handler until get pgvector working
-export { MemoryVectorStore } from 'langchain/vectorstores/memory';
