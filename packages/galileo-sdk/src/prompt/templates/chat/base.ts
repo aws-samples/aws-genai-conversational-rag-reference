e7bf2253926ef0c9d7f942d1b3398349d8b25658
Jeremy Jonas <jerjonas@amazon.com>
2023-11-07 17:12:27 +0800

    feat(prompt/adapter): simplify prompt adapter
    Remove extraneous template partial abstractions and
    Layout, replace with single "template" value and partials
    are common helpers only.

--- packages/galileo-sdk/src/prompt/templates/chat/base.ts
+++ packages/galileo-sdk/src/prompt/templates/chat/base.ts
@@ -6,58 +6,6 @@ import { HandlebarsTemplatePartials } from '../../handlebars.js';
 
 export interface BaseChatTemplatePartials extends HandlebarsTemplatePartials {
   /**
-   * Marker for denoting a set of content, such as chat history and corpus context.
-   * @default '''
-   */
-  readonly Delimiter: Handlebars.Template<any>;
-  /**
-   * Text for declaring the delimiter denotation. Such as, "the context denoted by '''"
-   * @default "denoted by '''" - With ''' equal to Delimiter partial value
-   */
-  readonly DelimitedBy: Handlebars.Template<any>;
-  /**
-   * Newline (soft break)
-   * @default \n
-   */
-  readonly LF: Handlebars.Template<any>;
-  /**
-   * Carriage return (hard break)
-   * @default \n\n
-   */
-  readonly CR: Handlebars.Template<any>;
-
-  /**
-   * Root layout of the template which composes the header, body, and footer partials.
-   */
-  readonly Layout: Handlebars.Template<any>;
-  /**
-   * Header is the beginning of the prompt and can be used to enforce specific markup
-   * before the main prompt as required by  LLM.
-   * @default empty
-   */
-  readonly Header: Handlebars.Template<any>;
-  /**
-   * Footer is the ending of the prompt and can be used to enforce specific markup at
-   * the end of every prompt as required by LLM.
-   * @default empty
-   */
-  readonly Footer: Handlebars.Template<any>;
-  /**
-   * Body is used for content layout of the instruction, context, and cue partials, and is
-   * placed between the header and footer.
-   */
-  readonly Body: Handlebars.Template<any>;
-  /**
-   * Context is the renderer for the chat history or corpus, which is the context of
-   * information used to direct the LLM.
-   */
-  readonly Context: Handlebars.Template<{ corpus?: any; chat_history?: BaseMessage[] }>;
-
-  /**
-   * Separator between messages.
-   */
-  readonly MessageSeparator: Handlebars.Template<any>;
-  /**
    * Human message in chat history
    */
   readonly HumanMessage: Handlebars.Template<HumanMessage>;
@@ -77,21 +25,8 @@ export interface BaseChatTemplatePartials extends HandlebarsTemplatePartials {
    * Renderer for chat history messages. Renders the specific message partials
    * based on the message type.
    */
-  readonly Messages: Handlebars.Template<{ messages: BaseMessage[] }>;
-  /**
-   * Layout and wrapper for chat history message dialog.
-   */
-  readonly Dialog: Handlebars.Template<{ messages: BaseMessage[] }>;
+  readonly ChatHistory: Handlebars.Template<{ messages: BaseMessage[] }>;
 
-  /**
-   * Renders the list of rules to instruct the LLM.
-   */
-  readonly Rules: Handlebars.Template<{ rules: string[] }>;
-
-  /**
-   * Renders the provided RAG search results used as knowledge for the LLM.
-   */
-  readonly Corpus: Handlebars.Template<{ corpus: any }>;
   // TODO: Document chains (Stuff, Reduce, etc) convert documents to string, but later will
   // submit a PR to support passing unprocessed list of source documents to the prompt
   // readonly Documents: Handlebars.Template<{ documents: Document[] }>;
@@ -99,40 +34,18 @@ export interface BaseChatTemplatePartials extends HandlebarsTemplatePartials {
 }
 
 export const BASE_CHAT_PARTIALS: BaseChatTemplatePartials = {
-  LF: '\n',
-  CR: '\n\n',
-  Delimiter: "'''",
-  DelimitedBy: ' delimited by {{>Delimiter}}',
-
-  Layout: '{{>Header}}{{>Body}}{{>Footer}}',
-  Header: '',
-  Footer: '',
-  Body: '{{>Instruction}}{{>CR}}{{>Context}}{{>CR}}{{>Cue}}',
-
-  Rules: '{{#each rules}}{{add @index 1}}. {{.}}{{#isnt @last true}}{{>LF}}{{/isnt}}{{/each}}',
-
-  MessageSeparator: '{{>LF}}',
   HumanMessage: 'Human: {{content}}',
   AIMessage: 'Assistant: {{content}}',
   SystemMessage: 'System: {{content}}',
   BaseMessage: '{{#if type}}{{type}}: {{/if}}{{content}}',
 
-  Messages: `{{#each chat_history}}
+  ChatHistory: `{{#each chat_history}}
 {{#if (eq type "human")}}{{>HumanMessage}}
 {{~else if (eq type "ai")}}{{>AIMessage}}
 {{~else if (eq type "system")}}{{>SystemMessage}}
 {{~else}}{{>BaseMessage}}
-{{/if}}{{#isnt @last true}}{{>MessageSeparator}}{{/isnt}}{{/each}}`,
-  Dialog: 'Dialog: {{>Delimiter}}{{>LF}}{{>Messages}}{{>LF}}{{>Delimiter}}',
-
-  Corpus: 'Corpus: {{>Delimiter}}{{>LF}}{{context}}{{>LF}}{{>Delimiter}}',
-
-  // determine "context" based on either corpus (qa) or dialog (condense)
-  // This is a helper to support cross-prompt scaffolding with common body
-  Context: '{{#if context}}{{>Corpus}}{{/if}}{{#if chat_history}}{{>Dialog}}{{/if}}',
+\n
+{{/each}}`,
 } as const;
 
-export interface ChatTemplatePartials extends BaseChatTemplatePartials {
-  readonly Instruction: Handlebars.Template<any>;
-  readonly Cue: Handlebars.Template<any>;
-}
+export interface ChatTemplatePartials extends BaseChatTemplatePartials {}
