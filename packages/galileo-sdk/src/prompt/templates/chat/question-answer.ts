64c94b7320253b561f919941b8cb363fc6e1658d
Jeremy Jonas <jerjonas@amazon.com>
2023-11-07 17:12:27 +0800

    feat(prompt/adapter): simplify prompt adapter
    Remove extraneous template partial abstractions and
    Layout, replace with single "template" value and partials
    are common helpers only.

--- packages/galileo-sdk/src/prompt/templates/chat/question-answer.ts
+++ packages/galileo-sdk/src/prompt/templates/chat/question-answer.ts
@@ -7,19 +7,24 @@ import {
   ScopedHandlebarsPromptTemplateInput,
 } from '../../handlebars.js';
 
-export const CHAT_QUESTION_ANSWER_PARTIALS: ChatTemplatePartials = {
-  ...BASE_CHAT_PARTIALS,
-  Context: '{{>Corpus}}',
-  Instruction:
-    'You are a research assistant in the "{{domain}}" domain. Based on the following rules and provided corpus {{~>DelimitedBy}}, answer the question. {{>CR}}Rules:{{>LF}}{{>Rules}}',
-  Cue: 'Question: {{question}}{{>LF}}Answer: ',
-} as const;
+export const CHAT_QUESTION_ANSWER_TEMPLATE = `You are a research assistant in the "{{domain}}" domain.
+Based on the following rules and provided corpus contained in ''' characters, answer the question.
+Rules:
+- only use knowledge from the provided corpus to answer the question
+- always be truthful, honest, unbiased, and unharmful
+- be concise, do not repeat the question or yourself in the answer
+
+Context: '''
+{{context}}
+'''
+
+Question: {{question}}
+Answer: `;
 
 export interface ChatQuestionAnswerPromptTemplateInputValues {
   readonly domain: string;
   readonly context: string;
   readonly question: string;
-  readonly rules?: string[];
 }
 
 export type ChatQuestionAnswerPromptTemplateInput = ScopedHandlebarsPromptTemplateInput<
@@ -38,20 +43,14 @@ export class ChatQuestionAnswerPromptTemplate extends HandlebarsPromptTemplate<
 
   constructor(input: ChatQuestionAnswerPromptTemplateInput) {
     super({
-      template: '{{>Layout}}',
+      template: CHAT_QUESTION_ANSWER_TEMPLATE,
       inputVariables: ['context', 'domain', 'question'],
       ...input,
       partialVariables: {
-        // LangChain partials only supports string or function return string... but actually can return anything
-        rules: [
-          'only use knowledge from the provided corpus to answer the question',
-          'always be truthful, honest, unbiased, and unharmful',
-          'be concise, do not repeat the question or yourself in the answer',
-        ],
         ...input.partialVariables,
       },
       templatePartials: {
-        ...CHAT_QUESTION_ANSWER_PARTIALS,
+        ...BASE_CHAT_PARTIALS,
         ...input.templatePartials,
       },
     });
