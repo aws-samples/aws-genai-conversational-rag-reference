cad3243a937bad8f2e508c2e52a5c690de40d73b
Gergely Varga <gergelyv@amazon.com>
2023-11-08 15:56:46 +0800

    feat(cli): add excel loader example

--- packages/galileo-cli/examples/csv-loader/metadata-provider-string.ts
+++ packages/galileo-cli/examples/csv-loader/metadata-provider-string.ts
@@ -1,10 +1,10 @@
 /*! Copyright [Amazon.com](http://amazon.com/), Inc. or its affiliates. All Rights Reserved.
 PDX-License-Identifier: Apache-2.0 */
 
-import * as fs from "node:fs";
-import * as path from "node:path";
-import { parseCsv } from "./parser-common";
-import { DocumentMetadata, IMetadataProvider } from "../../src";
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import { parseCsv } from './parser-common';
+import { DocumentMetadata, IMetadataProvider } from '../../src';
 
 export class MetadataProvider implements IMetadataProvider {
   async getMetadata(): Promise<string | DocumentMetadata> {
@@ -12,15 +12,15 @@ export class MetadataProvider implements IMetadataProvider {
     const docMetadata = parseCsv();
 
     // save it as a metadata.json file
-    const outputDir = path.join(__dirname, "generated");
-    const outputFile = path.join(outputDir, "metadata.json");
+    const outputDir = path.join(__dirname, 'generated');
+    const outputFile = path.join(outputDir, 'metadata.json');
 
     // create generated folder if doesn't exist
     fs.mkdirSync(outputDir, { recursive: true });
 
     // save into a file
     fs.writeFileSync(outputFile, JSON.stringify(docMetadata, null, 2), {
-      encoding: "utf-8",
+      encoding: 'utf-8',
     });
 
     // return absolute path
