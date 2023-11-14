728f1cea56159e5ff5355ab40f6d1280ae987039
Jeremy Jonas <jerjonas@amazon.com>
2023-11-14 16:31:45 +0800

    fix(website): browserify node natives from sdk

--- demo/website/.projen/react-config-overrides.js
+++ demo/website/.projen/react-config-overrides.js
@@ -4,6 +4,5 @@
  * @see https://www.npmjs.com/package/react-app-rewired
  */
 module.exports = function override(config, env) {
-  config.ignoreWarnings = [{"module":/node_modules\/(autolinker|ace-builds)/i}];
-  return config;
+  return require('../webpack/config-overrides')(config, env);
 };
