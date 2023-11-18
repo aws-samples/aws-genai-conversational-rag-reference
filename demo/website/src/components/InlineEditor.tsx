a74271796e7795e8d312875295540e3c4083f2e1
Gergely Varga <gergelyv@amazon.com>
2023-11-18 17:20:19 +0800

    feat(website): chat title update on enter

--- demo/website/src/components/InlineEditor.tsx
+++ demo/website/src/components/InlineEditor.tsx
@@ -4,7 +4,7 @@ import { Spinner } from '@cloudscape-design/components';
 import Button from '@cloudscape-design/components/button';
 import Input from '@cloudscape-design/components/input';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 type InlineEditorProps = {
   children: string;
@@ -18,19 +18,29 @@ export default function InlineEditor({ children, onChange, loading }: InlineEdit
   useEffect(() => {
     setValue(children);
   }, [children]);
+
+  const onSave = useCallback(() => {
+    setEditing(false);
+    onChange(value);
+  }, [setEditing, onChange, value]);
+
   return (
     <div style={{ display: 'flex' }}>
       {editing ? (
         <>
-          <Input onChange={({ detail }) => setValue(detail.value)} value={value} />
-          <Button
-            iconName="status-positive"
-            variant="inline-icon"
-            onClick={() => {
-              setEditing(false);
-              onChange(value);
+          <Input
+            onKeyDown={(e) => {
+              if (e.detail.key === 'Enter') {
+                onSave();
+                e.preventDefault();
+              }
             }}
+            onChange={({ detail }) => {
+              setValue(detail.value);
+            }}
+            value={value}
           />
+          <Button iconName="status-positive" variant="inline-icon" onClick={onSave} />
           <Button iconName="status-negative" variant="inline-icon" onClick={() => setEditing(false)} />
         </>
       ) : (
