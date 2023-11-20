e6c98bd7c053d2f8cc476885aa189c6b22c30c96
Jeremy Jonas <jerjonas@amazon.com>
2023-11-20 13:37:23 +0800

    chore(deps): upgrade managed dependencies (pdk,sdk,cdk,langhcain,etc)

--- projenrc/constants.ts
+++ projenrc/constants.ts
@@ -21,10 +21,10 @@ export const DEMO_APPLICATION_NAME = 'Galileo';
 
 /** Managed Dependency Versions */
 export const VERSIONS = {
-  AWS_SDK: '3.418.0',
-  CDK: '2.99.1',
-  CONSTRUCTS: '10.2.70',
-  PDK: '0.22.9',
-  LANGCHAIN: '0.0.157', // Not semver yet so need to pin version
-  SMITHY_TYPES: '2.3.4',
+  AWS_SDK: '3.451.0',
+  CDK: '2.110.0',
+  CONSTRUCTS: '10.3.0',
+  PDK: '0.22.42',
+  LANGCHAIN: '0.0.194', // Not semver yet so need to pin version
+  SMITHY_TYPES: '2.5.0',
 } as const;
